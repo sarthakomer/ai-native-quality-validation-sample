9d22ac4c5c77ab912e1b0184704d3c106d9821f6
Mudit Singh <43745884+muditlambda@users.noreply.github.com>
2025-12-02 21:49:51 +0530

    Remove London and Cairo, add Delhi to locations

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -24,10 +24,9 @@ const LOCATIONS = [
   { city: 'New Orleans', state: 'Louisiana', country: 'United States' },
   { city: 'Denver', state: 'Colorado', country: 'United States' },
   { city: 'Los Angeles', state: 'California', country: 'United States' },
-  { city: 'London', state: 'England', country: 'United States' },
   { city: 'Seoul', state: 'South Korea', country: 'South Korea' },
-  { city: 'Cairo', state: 'Egypt', country: 'United States' },
   { city: 'Ghaziabad', state: 'Uttar Pradesh', country: 'India' },
+  { city: 'Delhi', state: 'Delhi', country: 'India' },
 ];
 
 const SearchBar = ({ onSearch, initialFilters }: SearchBarProps) => {
